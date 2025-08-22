3c93c133185f04be5aacd90055adceb525f45187
Bolormaa <boloroots47@gmail.com>
2025-08-22 10:38:47 +0800

    remove bg color

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -5,24 +5,27 @@ import { Dispatch, SetStateAction } from "react";
 import { RichTextEditor } from "./richRextEditor";
 
 type HomePageProps = {
-  HandleDiary: ()=> void,
-  setDiary: Dispatch<SetStateAction<string>>,
-  diary:string,
-  loading:boolean
-}
-export const HomePage = ({ HandleDiary, diary, setDiary, loading}: HomePageProps) => {
-
-
+  HandleDiary: () => void;
+  setDiary: Dispatch<SetStateAction<string>>;
+  diary: string;
+  loading: boolean;
+};
+export const HomePage = ({
+  HandleDiary,
+  diary,
+  setDiary,
+  loading,
+}: HomePageProps) => {
   return (
-
-   <div>
-      <div className="min-h-screen flex flex-col items-center  bg-gradient-to-br from-blue-50 via-purple-50 to-pink-50 p-6 z-0">
+    <div>
+      <div className="min-h-screen flex flex-col items-center p-6 z-0">
         <div className="w-full max-w-md bg-white/60 backdrop-blur-xl rounded-2xl shadow-xl p-6">
           <div className="flex items-center gap-2 mb-4">
             <span className="w-3 h-3 rounded-full bg-pink-400"></span>
-            <h2 className="text-lg font-semibold">How&apos;s your day going?</h2>
+            <h2 className="text-lg font-semibold">
+              How&apos;s your day going?
+            </h2>
           </div>
-
 
           <RichTextEditor
             value={diary}
@@ -46,7 +49,8 @@ export const HomePage = ({ HandleDiary, diary, setDiary, loading}: HomePageProps
           disabled={loading}
           className="mt-6 px-10 py-3 rounded-full font-semibold text-white bg-gradient-to-r from-pink-300 via-pink-200 to-purple-300 shadow-[0_0_25px_rgba(236,72,153,0.3),0_0_40px_rgba(168,85,247,0.3)] hover:opacity-90 transition flex items-center justify-center gap-2"
         >
-          {loading && <Loader2 className="w-5 h-5 animate-spin" />}⚡ Analyze My Vibes ✨
+          {loading && <Loader2 className="w-5 h-5 animate-spin" />}⚡ Analyze My
+          Vibes ✨
         </Button>
       </div>
     </div>
