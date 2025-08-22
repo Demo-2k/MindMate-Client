4a262d097b1234159a4be7f5385aff1a795cd355
25LP3742 25LP3742 <Zoljargal.g247@gmail.com>
2025-08-22 11:10:47 +0800

    mhm

--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -18,8 +18,8 @@ export const HomePage = ({
 }: HomePageProps) => {
   return (
     <div>
-      <div className="min-h-screen flex flex-col items-center p-6 z-0">
-        <div className="w-full max-w-md bg-white/60 backdrop-blur-xl rounded-2xl shadow-xl p-6">
+      <div className="min-h-screen flex flex-col items-center pt-50 z-0">
+        <div className="w-screen max-w-md bg-white/60 backdrop-blur-xl rounded-2xl shadow-xl p-6">
           <div className="flex items-center gap-2 mb-4">
             <span className="w-3 h-3 rounded-full bg-pink-400"></span>
             <h2 className="text-lg font-semibold">
